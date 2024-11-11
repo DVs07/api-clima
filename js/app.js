bb5da6b76eb96ed61020526e7fa980718fee11dc
DVs07 <aldopablo7@hotmail.com>
2024-11-11 15:40:21 -0300

    Se agregaron las funcionalidades para consumir la API, mostrar la temperatura actual, maxima y minima.

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ function buscarClima(event){
     }
 
     // Consultar la API
+    consultarAPI(ciudad, pais);
 }
 
 function mostrarAlerta(mensaje) {
@@ -37,10 +38,10 @@ function mostrarAlerta(mensaje) {
         alerta.classList.add('bg-red-100','border-red-400','text-red-700', 'px-4','py-3','rounded','max-w-md','mx-auto','mt-6','text-center','border');
 
         alerta.innerHTML = `
-        <strong class="font-bold flex inline align-center justify-center">ERROR<svg class="w-6 h-6 dark:text-white mx-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <span class="flex inline align-center justify-center">
+        <strong class="font-bold flex inline "><svg class="w-6 h-6 dark:text-white mx-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
         <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd"/>
-        </svg></strong>
-        <span class="block">${mensaje}</span>
+        </svg>ERROR:</strong><span class="ml-2">${mensaje}</span></span>
         `
         // Agrego el alerta al DOM
         container.appendChild(alerta);
@@ -52,4 +53,66 @@ function mostrarAlerta(mensaje) {
         },3000);
     }
     
+}
+
+function consultarAPI(ciudad, pais) {
+    const appId = '9bdccbd5be1451751afe9d7a28c3d86c';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    // console.log(url);
+    
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            limpiarHTML();
+
+            if(datos.cod === '404'){
+                mostrarAlerta('Ciudad no encontrada');
+                return;
+            }
+
+            // Mostrar el resultado
+            mostrarClima(datos);
+        })
+}
+
+function mostrarClima(datos){
+    const {main:{temp, temp_max, temp_min}} = datos;
+
+    actual = convertirGrados(temp);
+    max = convertirGrados(temp_max);
+    min = convertirGrados(temp_min);
+
+
+    // Temperatura Actual
+    const tempActual = document.createElement('p');
+    tempActual.innerHTML = `${actual} &#8451`;
+    tempActual.classList.add('font-bold', 'text-6xl');
+
+    // Temperatura Maxima
+    const tempMax = document.createElement('p');
+    tempMax.innerHTML = `Max: ${max} &#8451`;
+    tempMax.classList.add('text-xl');
+
+    // Temperatura Minima
+    const tempMin = document.createElement('p');
+    tempMin.innerHTML = `Min: ${min} &#8451`;
+    tempMin.classList.add('text-xl');
+    
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(tempActual);
+
+    resultado.appendChild(resultadoDiv);
+    resultadoDiv.appendChild(tempMax);
+    resultadoDiv.appendChild(tempMin);
+}
+
+const convertirGrados = grados => parseInt(grados - 275.15);
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 }
